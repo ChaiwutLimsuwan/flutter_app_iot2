2db6482a448dc61f6e802d0c8471c380d0423467
ChaiwutLimsuwan <sau6119410009@sau.ac.th>
2021-10-14 10:12:51 +0700

    Code V.2 Do :WOW

---
+++ lib/views/register_ui.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class RegisterUI extends StatefulWidget {
+  const RegisterUI({Key? key}) : super(key: key);
+
+  @override
+  _RegisterUIState createState() => _RegisterUIState();
+}
+
+class _RegisterUIState extends State<RegisterUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
